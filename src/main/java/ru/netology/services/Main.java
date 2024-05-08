7c9eed50eaba93386165c64ae26285725e48ec35
Dmitrii Odynets <dimosaintp@gmail.com>
2024-05-08 10:42:48 +0300

    Причесал

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -5,15 +5,15 @@ public class Main {
 
         VacCalcService service = new VacCalcService();
 
-        //готовим данные
+        // данные для расчета
         int income = 100_000;
         int expenses = 60_000;
         int threshold = 150_000;
 
-        //вычисляем результат
+        // вычисляем результат
 
         int count = service.calculate(income, expenses, threshold);
 
-        System.out.println("Вам понадобится " + count + " месяцев");
+        System.out.println("Вам удастся отдохнуть " + count + " месяца");
     }
 }
--- src/main/java/ru/netology/services/VacCalcService.java
+++ src/main/java/ru/netology/services/VacCalcService.java
@@ -2,6 +2,7 @@ package ru.netology.services;
 
 public class VacCalcService {
 
+    // income — доход, expenses — обязательные месячные расходы, threshold — минимальная сумма необходимая для отдыха.
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха.
         int money = 0; // количество денег на счету.
@@ -9,12 +10,12 @@ public class VacCalcService {
         for (int month = 0; month < 12; month++) {
 
             if (money < threshold) {
-                money = money + income - expenses; // работаем.
+                money = money + income - expenses; // работа.
             } else {
-                money = (money - expenses) / 3; // отдыхаем.
+                money = (money - expenses) / 3; // отдых.
                 count++;
             }
         }
-        return count; // 1-я задача 3, 2-я задача 2.
+        return count;
     }
 }
